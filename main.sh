73a64d8859c4a2aced78505ac7e758b79bdd2a00
garner1 <silvano.garnerone@gmail.com>
2017-08-15 12:44:57 +0200

    creation of the corpus and word2vec

--- main.sh
+++ main.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
-echo "Create MC model ..." 	# approx 4m to run
-Rscript /home/garner1/Dropbox/pipelines/fastq2cloud/module_1/MC_model_from_fasta.R
+# echo "Create MC model ..." 	# approx 4m to run
+# Rscript /home/garner1/Dropbox/pipelines/fastq2cloud/module_1/MC_model_from_fasta.R
 
+# # GENERATE FASTQ FILES USING NEAT OR VARSIM_RUN                                                                                                                                              
+# # !!! check directories and parameters !!!!                                                                                                                                                 # # parallel "python ~/tools/neat-genreads/genReads.py -r ~/igv/genomes/hg19.fasta -R 50 -o ~/Work/dataset/genome_segmentation/simulated_data -c 1 --job {} 32" ::: `seq 32`
+# # python ~/tools/neat-genreads/mergeJobs.py -i  /home/garner1/tool_test/neat -o  /home/garner1/tool_test -s ~/tools/samtools-1.2
+
+# FASTQ.gz TO READS
 echo "Preapare reads ..."	# approx 14s to run
 bash ./module_2/parse_fastq.sh /home/garner1/Work/dataset/bliss/BB23_R1.fastq.gz
+echo "Done"
 
-echo "Prepare corpus ..."
-parallel python module_2/create_sentences.py {} ~/Work/dataset/fastq2cloud/transitionMatrix_3to3.csv ::: ~/Work/dataset/fastq2cloud/reads_a?
+# READS to CORPUS
+# These are pickle files TO BE USED IN GENSIM WORD2VEC sequentially, do not concatenate with cat
+echo "Prepare corpus ..."	# approx 2h with BB23_R1.fastq.gz
+parallel python module_2/create_corpus.py {} ::: ~/Work/dataset/fastq2cloud/reads/reads_*
+# python module_2/create_corpus.py ~/Work/dataset/fastq2cloud/reads/reads_test
+echo "Done"
+
+echo "Move corpus into specific directory"
+mkdir -p ~/Work/dataset/fastq2cloud/corpus
+mv ~/Work/dataset/fastq2cloud/reads/*_sentences.txt ~/Work/dataset/fastq2cloud/corpus
+echo "Done"
+
+echo 'Run gensim word2vec implementation ...'
+python ./module_3/word2vector.py ~/Work/dataset/fastq2cloud/corpus
+echo 'Done!
--- module_2/parse_fastq.sh
+++ module_2/parse_fastq.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 fastq=$1			# fastq.gz file as input
-zcat $fastq | paste - - - - | cut -f2 | split --lines=1000000 - /home/garner1/Work/dataset/fastq2cloud/reads_
+zcat $fastq | paste - - - - | cut -f2 | split --lines=100000 - /home/garner1/Work/dataset/fastq2cloud/reads/reads_
+
+# zcat $fastq | paste - - - - | cut -f2 | head -1000 > /home/garner1/Work/dataset/fastq2cloud/reads/reads_test
