4200a68b5c836f42d5e3958e418c758a6ab07282
garner1 <silvano.garnerone@gmail.com>
2017-08-17 09:38:47 +0200

    The way word1-word2 is counted is changed: now consider the order of occurrence and count word1-word1 as well

--- module_3/count_pairs.py
+++ module_3/count_pairs.py
@@ -16,12 +16,14 @@ corpus = pickle.load( open( file_name, "rb" ) )
 # Note that this will only work if the elements are in the same order in every list, 
 # e.g. Counter(chain.from_iterable(combinations(x,2) for x in [[1,2],[2,1]])) produces Counter({(1, 2): 1, (2, 1): 1}). 
 # If you want to count each pairing regardless of order, turn each list into a set first: 
-# Counter(chain.from_iterable(combinations(x,2) for x in [set([1,2]),set([2,1])])) produces Counter({(1, 2): 2}):
+# Counter(chain.from_iterable(combinations(x,2) for x in [set([1,2]),set([2,1])])) produces Counter({(1, 2): 2}).
+# BUT the set type will remove the diagonal counts. 
+# Keeping the list as is will count diagonals and in addition will remember the order of the word1-word2 pairs, originating a non-symmetric co-occurrence matrix, which might be OK
 ##########################
-# counts = Counter(chain.from_iterable(combinations(sentence, 2) for sentence in corpus))
-counts = Counter(chain.from_iterable(combinations(set(sentence), 2) for sentence in corpus))
+counts = Counter(chain.from_iterable(combinations(sentence, 2) for sentence in corpus))
+# counts = Counter(chain.from_iterable(combinations(set(sentence), 2) for sentence in corpus))
 
 with open(file_name + "_counter.txt",'w') as f:
-    for k,v in  counts.most_common():
+    for k,v in  counts_offdiag.most_common():
         f.write( "{} {}\n".format(k,v) )
 
