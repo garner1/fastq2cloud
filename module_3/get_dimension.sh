7ca111bbf66493fe2201f7b2c4105df62cb09d41
garner1 <silvano.garnerone@gmail.com>
2017-08-28 12:12:27 +0200

    minor commit

--- module_3/get_dimension.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo `cat /home/garner1/Work/dataset/fastq2cloud/corpus/reads_test_sentences.txt_counter.txt | tr -d '(),' | cut -d' ' -f-2 | tr ' ' '\n' | LC_ALL=C sort | LC_ALL=C uniq | wc -l`
