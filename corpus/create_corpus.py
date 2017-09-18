9646d9fd88691f962b697e2f8bdec8981f2533a5
garner1 <silvano.garnerone@gmail.com>
2017-09-18 10:15:10 +0200

    option to discard reads with N

--- corpus/create_corpus.py
+++ corpus/create_corpus.py
@@ -19,41 +19,48 @@ def thresholding_algo(y, lag, threshold, influence):
     filteredY = np.array(y)
     avgFilter = [0]*len(y)
     stdFilter = [0]*len(y)
-    avgFilter[lag - 1] = np.mean(y[0:2*lag])
-    stdFilter[lag - 1] = np.std(y[0:2*lag])
+    avgFilter[lag - 1] = np.mean(y[0:2*lag]) # set the mean to that of the window of 2lag size
+    stdFilter[lag - 1] = np.std(y[0:2*lag])  # set the std to that of the window of 2lag size
     for i in range(lag, len(y)-lag):
-        if (y[i] - avgFilter[i - 1]) > threshold * stdFilter [i-1] and y[i-1] < y[i]:
-            signals[i] = 1
-            filteredY[i] = influence * y[i] + (1 - influence) * filteredY[i-1]
-            avgFilter[i] = np.mean(filteredY[(i-lag):(i+lag)])
-            stdFilter[i] = np.std(filteredY[(i-lag):(i+lag)])
-        else:
-            signals[i] = 0
-            filteredY[i] = y[i]
-            avgFilter[i] = np.mean(filteredY[(i-lag):(i+lag)])
-            stdFilter[i] = np.std(filteredY[(i-lag):(i+lag)])
+        if (y[i] - avgFilter[i - 1]) > threshold * stdFilter [i-1] and y[i-1] < y[i]: # condition to call a stop word
+            signals[i] = 1      # call signal
+            filteredY[i] = influence * y[i] + (1 - influence) * filteredY[i-1] # update local data points
+            avgFilter[i] = np.mean(filteredY[(i-lag):(i+lag)]) # update the local mean
+            stdFilter[i] = np.std(filteredY[(i-lag):(i+lag)])  # update the local std
+        else:                                                  # if not stop word
+            signals[i] = 0                                     # is a word
+            filteredY[i] = y[i]                                # update local data
+            avgFilter[i] = np.mean(filteredY[(i-lag):(i+lag)]) # update local mean
+            stdFilter[i] = np.std(filteredY[(i-lag):(i+lag)])  # update local std
 
-    return dict(signals = np.asarray(signals),
-                avgFilter = np.asarray(avgFilter),
-                stdFilter = np.asarray(stdFilter))
+    return dict(signals = np.asarray(signals), # return list of words or stop words
+                avgFilter = np.asarray(avgFilter), # list of local averages
+                stdFilter = np.asarray(stdFilter)) # list of local stds
 
 
-chunck = str(sys.argv[1])
-model_file = str(sys.argv[2])   # '/media/DS2415_seq/silvanog/Genome_segmentation/transitionMatrix_3to3.csv'
+chunck = str(sys.argv[1])       # list of reads
+model_file = str(sys.argv[2])   # '/media/DS2415_seq/silvanog/Genome_segmentation/transitionMatrix_?to?.csv'
 dim = int(sys.argv[3])          # MC dimension
 column_names = ['kmer','information']
 model = pd.read_csv(model_file, sep=',', names=column_names)
+
 column_names = ['read']
 reads = pd.read_csv(chunck, header=None, names=column_names)
+
 sentences = []
 alphabet = ['C','G','A','T']
+
 # Settings parameters
-lag = 2*dim                     # half-window size where to consider local average and std
-threshold = 1                   # above this consider it a signal
-influence = 0.5                 # influence of the signals on the mooving average
+lag = 2*dim                     # window size where to consider local average and std
+threshold = 1                   # above this consider it a stop word
+influence = 0.5                 # influence of the stop-word on the mooving average
 sentences = []
+keep_N = True                  # if True randomly replace letter N; if False discard read
 for index, row in reads.iterrows():
-    read = row['read'].replace('N',random.choice(alphabet)) # random substitution of N letter (OR IS IT BETTER TO DISCARD THE READS?)
+    if keep_N :
+        read = row['read'].replace('N',random.choice(alphabet)) # random substitution of letter N (OR IS IT BETTER TO DISCARD THE READS?)
+    else:
+        continue                # discard this read
     kmers = pd.DataFrame([read[i:i+2*dim] for i in range(len(read)-(2*dim-1))]) # create list of kmers from read
     kmers.columns = ['kmer']
     infodata = kmers.merge(model, on='kmer', how='left').fillna(0) # JOIN KMERS DF AND ENTROPY VALUES DF(model)
